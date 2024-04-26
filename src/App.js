f6287fe59ca31932fd2da4513b678a0916f10af7
global_gifts <fifa.1998.sava@mail.ru>
2024-04-26 13:54:36 +1000

    второй блок готов

--- src/App.js
+++ src/App.js
@@ -51,11 +51,11 @@ function App() {
               </div>
             </div>
           </div>
-          {/* <div className={styles.main__contain_card}>
+          <div className={styles.main__contain_card}>
             {cards.map((card) => (
               <Card key={`card_${card.id}`} card={card} />
             ))}
-          </div> */}
+          </div>
           {/* <div className={styles.main__content}>
             {cardsCarSeller.map((card) => (
               <CardSeller key={`card_${card.id}`} card={card} />
